bb0634941658c6816298fbcd3799488d9d9d7d6a
Koreyoshii <676508816@qq.com>
2017-05-23 00:12:51 +0800

    Update MallardDuck.java

--- src/model/MallardDuck.java
+++ src/model/MallardDuck.java
@@ -1,7 +1,21 @@
 package model;
 
-public class MallardDuck extends Duck {//驯化野鸭
-	public void display(){
-		System.out.println("MallardDuck display");
-	};
+public class MallardDuck extends Duck {
+	public MallardDuck() {
+		QuackBehavior = new Quack();
+		FlyBehavior = new FlyWithWings();
+	}
+
+	@Override
+	public void swim() {
+		// TODO Auto-generated method stub
+		System.out.println("MallardDuck会游泳");
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("这只鸭子是MallardDuck");
+	}
+
 }
