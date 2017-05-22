82ea0de8d8ec8fac88646ffabc55742e1f3352d4
Koreyoshii <676508816@qq.com>
2017-05-23 00:15:21 +0800

    Update RedHatDuck.java

--- src/model/RedHatDuck.java
+++ src/model/RedHatDuck.java
@@ -1,7 +1,21 @@
 package model;
 
 public class RedHatDuck extends Duck {//驯化野鸭
-	public void display(){
-		System.out.println("RedHatDuck display");
-	};
+	public RedHeadDuck() {
+		QuackBehavior = new MuteQuack();
+		FlyBehavior = new FlyNoWay();
+	}
+
+	@Override
+	public void swim() {
+		// TODO Auto-generated method stub
+		System.out.println("RedHeadDuck会游泳");
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("这只鸭子是RedHeadDuck");
+	}
+
 }
