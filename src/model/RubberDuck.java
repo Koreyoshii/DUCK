041cd4797680d17839a3f23982d12f22578da62a
Koreyoshii <676508816@qq.com>
2017-05-23 00:16:31 +0800

    Update RubberDuck.java

--- src/model/RubberDuck.java
+++ src/model/RubberDuck.java
@@ -1,7 +1,21 @@
 package model;
 
 public class RubberDuck extends Duck {//橡皮鸭
-	public void display(){
-		System.out.println("RubberDuck display");
-	};
+	public RubberDuck() {
+		QuackBehavior = new Squeak();
+		FlyBehavior = new FlyWithRocket();
+	}
+
+	@Override
+	public void swim() {
+		// TODO Auto-generated method stub
+		System.out.println("RubberDuck会游泳");
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("这只鸭子是RubberDuck");
+	}
+
 }
